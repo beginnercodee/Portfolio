ed79ff16130e127e23bb338712fa0e9425dff793
Jamal Nadeem <jamalnadeem2004@gmail.com>
2026-05-13 23:12:59 +0500

    feat: test commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      // Prevent crawlers from indexing internal or private routing paths if needed
+      //Prevent crawlers from indexing internal or private routing paths if needed
       disallow: ['/private/'],
     },
     sitemap: 'https://jamalnadeem.com/sitemap.xml',
