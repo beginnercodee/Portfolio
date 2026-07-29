9fe2ad3bf98df3a1c97a0823689f4507129207d8
Jamal Nadeem <jamalnadeem2004@gmail.com>
2026-07-29 12:10:41 +0500

    docs: add JSDoc comments to cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines and merges conditional Tailwind CSS class names using `clsx` and `tailwind-merge`.
+ * Eliminates conflicting or duplicate utility classes efficiently.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
