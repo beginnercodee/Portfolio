982f1f2f1ca4b3c15f69d5c9bed64129ed06edaf
Jamal Nadeem <jamalnadeem2004@gmail.com>
2026-08-10 13:12:28 +0500

    docs: add JSDoc comments to robots function

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,9 @@
 import { MetadataRoute } from 'next';
 
+/**
+ * Generates the robots.txt metadata configuration for web crawlers,
+ * defining crawler access rules and linking the XML sitemap location.
+ */
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
